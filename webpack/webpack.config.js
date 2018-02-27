9f62873c5c26bf08b705a9042a3b6a05dde6c401
hcam05 <hcam05@gmail.com>
2018-02-26 21:54:59 -0800

    gitignore and webpack setup

---
+++ webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: path.join(__dirname, '../app/app'),
+  entry: [
+    '../app.js',
+  ],
+  output: {
+    path: path.join(__dirname, '../www/assets'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, '../node_modules'),
+    ],
+  },
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   names: ['vendor', 'manifest']
+    // }),
+    new HtmlWebpackPlugin({
+      template: '../../www/index.html'
+    })
+  ],
+};
