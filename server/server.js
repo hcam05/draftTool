a1bf0971cfd02ea5799e4281ce8375ad8bf6a8ee
hcam05 <hcam05@gmail.com>
2018-02-26 21:54:49 -0800

    app set up

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack/webpack.config.js');
+const app = express();
+const mcache = require('memory-cache');
+
+//CACHE//
+let cache = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key);
+
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.end
+      res.end = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+      }
+      next();
+    }
+  }
+}
+
+const compiler = webpack(webpackConfig);
+ 
+app.use(express.static(__dirname + '/www'));
+ 
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+ 
+const server = app.listen(9000, function() {
+  const port = server.address().port;
+  console.log(`Is it over ${port}?!?!`);
+});
