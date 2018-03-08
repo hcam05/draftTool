6f03ad3fe1069a830546ea1bff2c5bdd1d627638
hcam05 <hcam05@gmail.com>
2018-03-07 20:36:16 -0800

    store initial setup

---
+++ app/redux/store/configureStore.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers/reducers';
+import { createLogger } from 'redux-logger';
+import { getAllProducts } from '../actions/actions';
+
+const configureStore = () => {
+
+  const middleware = [thunk];
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+  }
+
+  const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+  );
+
+  // store.dispatch(getAllProducts());
+  
+  return store;
+}
+
+
+export default configureStore;
