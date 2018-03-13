5642ca4e8a3d9ed9b6e2491ef839060aa998d808
hcam05 <hcam05@gmail.com>
2018-03-12 17:44:11 -0700

    draft functionality working

--- app/containers/Draft.jsx
+++ app/containers/Draft.jsx
@@ -6,30 +6,45 @@ import { connect } from 'react-redux'
 import PlayerCell from '../components/PlayerCell.jsx';
 import DraftedPlayers from '../containers/DraftedPlayers.jsx';
 
-import { selectPlayer } from '../redux/actions/actions';
+import { selectPlayer } from '../redux/actions/index';
 
-const Draft = ({ players, selectPlayer }) => {
+const Draft = ({ players, draftedPlayers, selectPlayer }) => {
   // console.log('selectPlayer from Draft container:',selectPlayer)
   let avaliablePlayers = players.map((x) => {
-    return <PlayerCell
-      key={x.id}
-      player={x}
-      onSinglePlayerClicked={() => selectPlayer(x.id)}
-    />
+    // console.log('x: ',x);
+    if (x.drafted === false) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
   })
+
+  let playersDrafted = draftedPlayers.map((x) => {
+    if (x.drafted === true) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
+  });
 
   return (
 
     <div players={players}>
       Avaliable Players
       {(players.length > 0) ? avaliablePlayers : "Loading"}
-      <DraftedPlayers />
+      Drafted Players
+      {playersDrafted}
     </div>
   )
 }
 
 const mapStateToProps = (state) => ({
-  players: state.loadAllDraftPlayers
+  players: state.loadAllDraftPlayers,
+  draftedPlayers: state.draftPlayer,
 });
 
 export default connect(
