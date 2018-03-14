38ace0c026b7f381be135bcab7d6b7a5a6ad9c03
hcam05 <hcam05@gmail.com>
2018-03-14 15:10:06 -0700

    added lists of players by position

--- app/containers/Draft.jsx
+++ app/containers/Draft.jsx
@@ -2,27 +2,13 @@ import React from 'react';
 import axios from 'axios';
 
 import { connect } from 'react-redux'
-
 import PlayerCell from '../components/PlayerCell.jsx';
-import DraftedPlayers from '../containers/DraftedPlayers.jsx';
+import DraftedPlayers from './DraftedPlayers.jsx';
+import { selectPlayer, undoDraft } from '../redux/actions/index';
 
-import { selectPlayer } from '../redux/actions/index';
-
-const Draft = ({ players, draftedPlayers, selectPlayer }) => {
-  // console.log('selectPlayer from Draft container:',selectPlayer)
+const Draft = ({ players, draftedPlayers, selectPlayer, undoDraft }) => {
   let avaliablePlayers = players.map((x) => {
-    // console.log('x: ',x);
     if (x.drafted === false) {
-      return <PlayerCell
-        key={x.id}
-        player={x}
-        onSinglePlayerClicked={() => selectPlayer(x)}
-      />
-    }
-  })
-
-  let playersDrafted = draftedPlayers.map((x) => {
-    if (x.drafted === true) {
       return <PlayerCell
         key={x.id}
         player={x}
@@ -31,23 +17,87 @@ const Draft = ({ players, draftedPlayers, selectPlayer }) => {
     }
   });
 
+  let playersDrafted = draftedPlayers.map((x) => {
+    if (x.drafted === true) {
+      return <DraftedPlayers
+        key={x.id}
+        player={x}
+      />
+    }
+  });
+
+  let allRB = players.map((x) => {
+    if (x.position === 'RB' && x.drafted === false) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
+  });
+
+  let allWR = players.map((x) => {
+    if (x.position === 'WR' && x.drafted === false) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
+  });
+
+  let allQB = players.map((x) => {
+    if (x.position === 'QB' && x.drafted === false) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
+  });
+
+  let allTE = players.map((x) => {
+    if (x.position === 'TE' && x.drafted === false) {
+      return <PlayerCell
+        key={x.id}
+        player={x}
+        onSinglePlayerClicked={() => selectPlayer(x)}
+      />
+    }
+  });
+
+
   return (
 
     <div players={players}>
       Avaliable Players
       {(players.length > 0) ? avaliablePlayers : "Loading"}
+      QB
+      {(players.length > 0) ? allQB : "Loading"}
+      WR
+      {(players.length > 0) ? allWR : "Loading"}
+      RB
+      {(players.length > 0) ? allRB : "Loading"}
+      TE
+      {(players.length > 0) ? allTE : "Loading"}
       Drafted Players
+      <button onClick={() => playersDrafted.length > 0
+        ? undoDraft(playersDrafted[playersDrafted.length - 1].props.player)
+        : console.log('no players drafted')}>
+        Undo
+        </button>
       {playersDrafted}
     </div>
   )
 }
 
 const mapStateToProps = (state) => ({
-  players: state.loadAllDraftPlayers,
+  players: state.allPlayers,
   draftedPlayers: state.draftPlayer,
+  undoDraft: state.undoDraft
 });
 
 export default connect(
   mapStateToProps,
-  { selectPlayer }
+  { selectPlayer, undoDraft }
 )(Draft);
