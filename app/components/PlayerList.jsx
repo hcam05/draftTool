fe05bb1d20907f406dcf3a7eeca81aaaf274106a
hcam05 <hcam05@gmail.com>
2018-03-19 22:23:21 -0700

    working on code modularization

---
+++ app/components/PlayerList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PlayerList = (postion) => {
+
+  return (
+    <div className={`all${postion}`}>
+      <span className='draftBoard-header'>
+        {/* {position} */}
+      </span>
+      {(players.length > 0) ? `all${position}` : "Loading"}
+    </div>
+  )
+}
+
+export default PlayerList;
