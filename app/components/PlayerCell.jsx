78dded7242f511e5d894cb0740329e56877d34ec
hcam05 <hcam05@gmail.com>
2018-03-19 22:21:17 -0700

    updated css to use flexbox in playercell

--- app/components/PlayerCell.jsx
+++ app/components/PlayerCell.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import '../styles/css/playerCell.css';
+import '../styles/css/playercell.css';
 
 const PlayerCell = (props) => {
   const { onSinglePlayerClicked, player, positionRank } = props;
   return (
     <div onClick={onSinglePlayerClicked} className='playerCell'>
-      Rank:&nbsp;{player.rank}&nbsp;
+      <span className='playerCell-rank'>
+        Rank:&nbsp;{player.rank}&nbsp;
+      </span>
+      <span className='playerCell-name'>
       {player.firstName}&nbsp;{player.lastName}&nbsp;
+      </span>
+      <span className='playerCell-position'>
       {player.position}&nbsp;
+      </span>
+      <span className='playerCell-team'>
       {player.team}
+      </span>
     </div>
   )
 }
