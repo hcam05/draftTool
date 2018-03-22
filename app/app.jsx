ddf644afeab4e36ad72eac755e8dd85ad917acd4
hcam05 <hcam05@gmail.com>
2018-03-21 23:42:25 -0700

    implement react router

--- app/app.jsx
+++ app/app.jsx
@@ -6,16 +6,32 @@ import 'babel-polyfill';
 import configureStore from './redux/store/configureStore';
 
 import DraftTool from './containers/DraftTool.jsx';
+import DraftBoard from './containers/DraftBoard.jsx';
+import DraftSetup from './components/DraftSetup.jsx';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+
+const history = createHistory();
+
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link
+// } from 'react-router-dom'
 
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <DraftTool />
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={DraftTool} />
+        <Route path="/board" component={DraftBoard} />
+        <Route path="/setup" component={DraftSetup} />
+      </div>
+    </ConnectedRouter>
+    {/* <DraftTool /> */}
   </Provider>,
   document.getElementById('mount'));
