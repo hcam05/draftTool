a202450ecf7cdf71376f434f317a31e35c86551e
hcam05 <hcam05@gmail.com>
2018-03-21 23:43:50 -0700

    setting up router links for development

--- app/components/DraftSetup.jsx
+++ app/components/DraftSetup.jsx
@@ -1,46 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const DraftSetup = ({ numTeams, numPlayers, teamNames, setNumTeams, setNumPlayerSlots, setAllTeamNames }) => {
+import { Link } from 'react-router-dom';
+import { incrementTeams, decrementTeams, incrementPlayers, decrementPlayers, setAllTeamNames } from '../redux/actions/index';
 
-  const numOptions = () => {
-    let optionsArray = []
-    for (let i = 0; i < 20; i += 1) {
-      optionsArray.push(
-        <option value="i">i</option>
-      )
-      return optionsArray;
-    }
-  }
+import '../styles/css/draftSetup.css';
 
+const DraftSetup = ({ numTeams, numPlayers, teamNames, incrementTeams, decrementTeams, incrementPlayers, decrementPlayers, setAllTeamNames }) => {
 
   return (
     <div className='draftSetup'>
       {/* Set Teams */}
-      <select name='numTeams' onSelect={() => setNumTeams()}>
-        {numOptions}
-      </select>
-      {/* Set Number of Players */}
-      <select name="numPlayers" onSelect={()=> setNumPlayerSlots()}>
-        {numOptions}
-      </select>
       {/* Set Teams */}
-      Set Teams
+      <Link to='/'>Draft Tool</Link>
+      <br />
+      <Link to='/board'>Board</Link>
+      <br />
+      Number of Teams &nbsp;
+      <a className='draftSetup-button' onClick={() => decrementTeams()}>-</a> {numTeams} <a className='draftSetup-button' onClick={() => incrementTeams()}>+</a>
+      <br />
+
+      {/* Set Number of Players */}
+      Total Players &nbsp;
+        <a className='draftSetup-button' onClick={() => decrementPlayers()}>-</a> {numPlayers} <a className='draftSetup-button' onClick={() => incrementPlayers()}>+</a>
+      <br />
     </div>
   )
 }
 
-const mapStateToProps = (state)  => ({
+const mapStateToProps = (state) => ({
   numTeams: state.numTeams,
   numPlayers: state.playerSlots,
   teamNames: state.teamNames
 });
 
 export default connect(
-  mapStateToProps
-  // {
-  //   setNumTeams,
-  //   setNumPlayerSlots,
-  //   setAllTeamNames
-  // }
+  mapStateToProps,
+  {
+    incrementTeams,
+    decrementTeams,
+    incrementPlayers,
+    decrementPlayers,
+    setAllTeamNames
+  }
 )(DraftSetup);
